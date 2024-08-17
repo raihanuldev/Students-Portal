69367ead57cb26855f3611ffb87e3d2284781a80
raihanuldev <rihanthedev@gmail.com>
2024-08-17 14:15:57 +0600

    planing for project

---
+++ src/utlities/TODO.ts
@@ -0,0 +1,22 @@
+/*
+ * this is will an slice
+ * addStudent reducer
+ * deleteStudent Reducer
+ * updateStudent Reducer
+ * 
+ * 
+ * 
+ * 
+ * TODO:=> Create a Backend Server useing NodeJs ExpressJs Where have intially CRUD
+ * create=> POSTStudent
+ * Read => get Stduents
+ * Update Student
+ * delete Student
+ * 
+ * intrgrate RTK Query For Post Student
+ * intrgrate GET Student
+ * update Stuednt
+ * delete Student
+ * 
+ * Student
+ */
