f17518e6efb279c8603f172d14df069b359317ad
raihanuldev <rihanthedev@gmail.com>
2024-08-18 02:34:20 +0600

    ManageStudent

--- src/pages/ManageStudents.tsx
+++ src/pages/ManageStudents.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hook';
 
 const ManageStudents = () => {
     const students = useAppSelector((state) => state.students);
-  
       console.log(students);
+
     return (
         <div>
             
