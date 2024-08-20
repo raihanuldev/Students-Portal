f0e0dec2248929442d207bccfbcd8d18878e845e
raihanuldev <rihanthedev@gmail.com>
2024-08-20 07:48:25 +0600

    intrgrate with store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import StudentSlice from './features/StudentSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import StudentSlice from "./features/StudentSlice";
+import { baseApi } from "./api/baseApi";
 // ...
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     students: StudentSlice,
   },
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
