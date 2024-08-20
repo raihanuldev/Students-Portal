a46bae563287fd6930fabde26a7418185804ef4a
raihanuldev <rihanthedev@gmail.com>
2024-08-20 08:47:02 +0600

    fix buildError

--- src/pages/ManageStudents.tsx
+++ src/pages/ManageStudents.tsx
@@ -10,20 +10,10 @@ interface DataType {
   department: string;
   phone: string;
 }
-// Data-Type for the API response
-interface Student {
-  _id: string;
-  studentName: string;
-  roll: number;
-  address: string;
-  department: string;
-  phone: string;
-}
+
 const ManageStudents = () => {
   const { data: students, isFetching } = useGetStudentsQuery(undefined);
-  console.log(students);
 
-  // handleUpdate
   const handleUpdate = (data: DataType) => {
     console.log(data);
   };
@@ -32,44 +22,31 @@ const ManageStudents = () => {
     {
       title: "Student Name",
       dataIndex: "studentName",
-      showSorterTooltip: { target: "full-header" },
-      onFilter: (value, record) =>
-        record.studentName.indexOf(value as string) === 0,
       sorter: (a, b) => a.studentName.length - b.studentName.length,
       sortDirections: ["descend"],
+      onFilter: (value, record) =>
+        record.studentName.indexOf(value as string) === 0,
     },
     {
       title: "Roll",
       dataIndex: "roll",
-      defaultSortOrder: "descend",
       sorter: (a, b) => a.roll - b.roll,
+      defaultSortOrder: "descend",
     },
     {
       title: "Department",
       dataIndex: "department",
       filters: [
-        {
-          text: "Computer",
-          value: "computer",
-        },
-        {
-          text: "Electrical",
-          value: "electrical",
-        },
+        { text: "Computer", value: "computer" },
+        { text: "Electrical", value: "electrical" },
       ],
     },
     {
       title: "Address",
       dataIndex: "address",
       filters: [
-        {
-          text: "London",
-          value: "London",
-        },
-        {
-          text: "New York",
-          value: "New York",
-        },
+        { text: "London", value: "London" },
+        { text: "New York", value: "New York" },
       ],
       onFilter: (value, record) =>
         record.address.indexOf(value as string) === 0,
@@ -85,7 +62,7 @@ const ManageStudents = () => {
   ];
 
   const tableData: DataType[] | undefined = students?.map(
-    ({ _id, studentName, roll, department, phone, address }) => ({
+    ({ _id, studentName, roll, department, phone, address }:any) => ({
       key: _id,
       studentName,
       roll,
@@ -110,7 +87,6 @@ const ManageStudents = () => {
       loading={isFetching}
       dataSource={tableData}
       onChange={onChange}
-      showSorterTooltip={{ target: "sorter-icon" }}
     />
   );
 };
