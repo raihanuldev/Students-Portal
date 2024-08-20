9612c30d70e0cfa3399f871fb2cfb07804de1f3e
raihanuldev <rihanthedev@gmail.com>
2024-08-20 09:09:28 +0600

    prod link added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://student-portal-backend.vercel.app" }),
   endpoints: (builder) => ({
     addStudent: builder.mutation({
       query: (data) => ({
